4261ac8592e7ea3180121b886a703177cdad3933
Santiago Vélez Pulgarin <santiago.velez@ceiba.com.co>
2021-08-15 17:21:01 -0500

    Se actualizan rutas de scripts y base de datos, el listar funciona en este commit

--- microservicio/src/main/resources/db/migration/DDL/V2.0__schema.sql
+++ microservicio/src/main/resources/db/migration/DDL/V2.0__schema.sql
@@ -1,7 +1,7 @@
 create table suscripcion (
  idSuscripcion int(11) not null auto_increment,
  idCliente int(11) not null,
- valorSuscripcion decimal(5,2) not null,
+ valorSuscripcion decimal(19,2) not null,
  tipoSuscripcion varchar(30) not null,
  fechaRegistro datetime null,
  primary key (idSuscripcion)
