3cc93211330aed1e6898b70d55ed068f9dca5667
Santiago Vélez Pulgarin <santiago.velez@ceiba.com.co>
2021-08-15 12:04:37 -0500

    Se agrega anotacion @component para la aplicación e infraestructura, con el fin de que spring pueda crear los beans inyectados

--- microservicio/aplicacion/src/main/java/com/ceiba/suscripcion/consulta/ManejadorListarSuscripcion.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/suscripcion/consulta/ManejadorListarSuscripcion.java
@@ -2,9 +2,11 @@ package com.ceiba.suscripcion.consulta;
 
 import com.ceiba.suscripcion.modelo.dto.DtoSuscripcion;
 import com.ceiba.suscripcion.puerto.dao.DaoSuscripcion;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class ManejadorListarSuscripcion {
 
     private DaoSuscripcion daoSuscripcion;
--- microservicio/infraestructura/src/main/java/com/ceiba/suscripcion/adaptador/dao/DaoSuscripcionMysql.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/suscripcion/adaptador/dao/DaoSuscripcionMysql.java
@@ -4,9 +4,11 @@ import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
 import com.ceiba.infraestructura.jdbc.sqlstatement.SqlStatement;
 import com.ceiba.suscripcion.modelo.dto.DtoSuscripcion;
 import com.ceiba.suscripcion.puerto.dao.DaoSuscripcion;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class DaoSuscripcionMysql implements DaoSuscripcion {
 
     private final CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate;
