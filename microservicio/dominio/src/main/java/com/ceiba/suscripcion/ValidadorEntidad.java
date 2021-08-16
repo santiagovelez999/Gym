9b676a8f209317320da5a7098abd23f9c15c5ad5
Santiago Vélez Pulgarin <santiago.velez@ceiba.com.co>
2021-08-16 12:37:40 -0500

    Se agregan validaciones a la entidad suscripcion

--- microservicio/dominio/src/main/java/com/ceiba/suscripcion/ValidadorEntidad.java
+++ microservicio/dominio/src/main/java/com/ceiba/suscripcion/ValidadorEntidad.java
@@ -3,6 +3,10 @@ package com.ceiba.suscripcion;
 import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class ValidadorEntidad {
 
@@ -10,7 +14,9 @@ public class ValidadorEntidad {
     private static final String TIPO_SUSCRIPCION_POR_QUINCENA = "XV";
 
     private static final BigDecimal VALOR_SUSCRIPCION_MENSUAL = new BigDecimal(70000.00);
-    private static final BigDecimal VALOR_SUSCRIPCION_QUINCENAL = new BigDecimal(40000.00);;
+    private static final BigDecimal VALOR_SUSCRIPCION_QUINCENAL = new BigDecimal(40000.00);
+
+    private static final String[] DIAS_SEMANA_DESCUENTO = {"WEDNESDAY","THURSDAY"};
 
     private ValidadorEntidad(){};
 
@@ -26,5 +32,32 @@ public class ValidadorEntidad {
                 !valorSuscripcion.equals(VALOR_SUSCRIPCION_QUINCENAL)) {
             throw new ExcepcionValorInvalido(mensaje);
         }
+    }
+
+    public static void validarRelacionTipoSuscripcionYValorSuscripcion(String tipoSuscripcion, BigDecimal valorSuscripcion, String mensaje){
+        if(tipoSuscripcion.equals(TIPO_SUSCRIPCION_POR_MES)){
+            if(!valorSuscripcion.equals(VALOR_SUSCRIPCION_MENSUAL)){
+                throw new ExcepcionValorInvalido(mensaje);
+            }
+        }else if(tipoSuscripcion.equals(TIPO_SUSCRIPCION_POR_QUINCENA)){
+            if(!valorSuscripcion.equals(VALOR_SUSCRIPCION_QUINCENAL)){
+                throw new ExcepcionValorInvalido(mensaje);
+            }
+        }
+    }
+
+    public static BigDecimal validarDescuentoValorSuscripcion(String tipoSuscripcion, BigDecimal valorSuscripcion, LocalDateTime fechaRegistro){
+        if(tipoSuscripcion.equals(TIPO_SUSCRIPCION_POR_MES)){
+            List<String> listDays = Arrays.asList(DIAS_SEMANA_DESCUENTO);
+            System.out.println("FECHA: "+ fechaRegistro.getDayOfWeek());
+            System.out.println("VALIDACION: " + listDays.contains(fechaRegistro.getDayOfWeek().toString()));
+            if(listDays.contains(fechaRegistro.getDayOfWeek().toString())){
+                 System.out.println("SE HACE DESCUENTO");
+                 BigDecimal valorConDescuento = new BigDecimal((VALOR_SUSCRIPCION_MENSUAL.intValueExact()*7)/100);
+                System.out.println(valorConDescuento);
+            }
+            System.out.println("NO HACE DESCUENTO");
+        }
+        return valorSuscripcion;
     }
 }
--- microservicio/dominio/src/main/java/com/ceiba/suscripcion/modelo/entidad/Suscripcion.java
+++ microservicio/dominio/src/main/java/com/ceiba/suscripcion/modelo/entidad/Suscripcion.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import static com.ceiba.dominio.ValidadorArgumento.validarObligatorio;
 import static com.ceiba.suscripcion.ValidadorEntidad.validarTipoSuscripcion;
 import static com.ceiba.suscripcion.ValidadorEntidad.validarValorSuscripcion;
+import static com.ceiba.suscripcion.ValidadorEntidad.validarRelacionTipoSuscripcionYValorSuscripcion;
+import static com.ceiba.suscripcion.ValidadorEntidad.validarDescuentoValorSuscripcion;
 
 @Getter
 public class Suscripcion {
@@ -16,6 +18,7 @@ public class Suscripcion {
     private static final String SE_DEBE_INGRESAR_VALOR_DE_SUSCRIPCION = "Se debe ingresar el valor de la suscripción";
     private static final String TIPO_SUSCRIPCION_INVALIDO = "El tipo de suscripción es invalido";
     private static final String VALOR_SUSCRIPCION_INVALIDO = "El valor de la suscripción no es valido";
+    private static final String VALOR_TIPO_SUSCRIPCION_Y_VALOR_SUSCRIPCION_INVALIDO = "El valor de la suscripción no es valido para el tipo de suscripción";
 
     private Long idSuscripcion;
     private Long idCliente;
@@ -30,6 +33,9 @@ public class Suscripcion {
         validarObligatorio(valorSuscripcion, SE_DEBE_INGRESAR_VALOR_DE_SUSCRIPCION);
         validarTipoSuscripcion(tipoSuscripcion, TIPO_SUSCRIPCION_INVALIDO);
         validarValorSuscripcion(valorSuscripcion, VALOR_SUSCRIPCION_INVALIDO);
+        validarRelacionTipoSuscripcionYValorSuscripcion(tipoSuscripcion, valorSuscripcion,
+                VALOR_TIPO_SUSCRIPCION_Y_VALOR_SUSCRIPCION_INVALIDO);
+        validarDescuentoValorSuscripcion(tipoSuscripcion, valorSuscripcion, fechaRegistro);
 
         this.idSuscripcion = idSuscripcion;
         this.idCliente = idCliente;
