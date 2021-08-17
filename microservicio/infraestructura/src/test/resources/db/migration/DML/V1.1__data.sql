b323472ce4b5b82997a05a6995cc32543fddbc4d
Santiago Vélez Pulgarin <santiago.velez@ceiba.com.co>
2021-08-17 14:12:13 -0500

    Se agregan pruebas de controlador para el listar de suscripciones

--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,1 +1,3 @@
-insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now())
+insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now());
+
+insert into suscripcion(idCliente,valorSuscripcion,tipoSuscripcion,fechaRegistro)values(50,70000,'XXX',now());
--- microservicio/src/main/resources/db/migration/DDL/V2.0__schema.sql
+++
@@ -1,8 +0,0 @@
-create table suscripcion (
- idSuscripcion int(11) not null auto_increment,
- idCliente int(11) not null,
- valorSuscripcion decimal(19,2) not null,
- tipoSuscripcion varchar(30) not null,
- fechaRegistro datetime null,
- primary key (idSuscripcion)
-);
